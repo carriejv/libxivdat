248e8cc697813670a23c41b83edfbd0a92daa8f9
Carrie J V <carrie@carriejv.com>
2021-07-13 20:33:37 -0700

    Implement MacroData

--- src/high_level/macro.rs
+++ src/high_level/macro.rs
@@ -7,7 +7,6 @@ use icon::*;
 
 use crate::dat_error::DATError;
 use crate::section::{as_section, as_section_vec, Section, SectionData};
-use std::convert::{TryFrom, TryInto};
 
 /// The [`Section`](crate::section::Section) tag for macro titles.
 pub const SECTION_TAG_TITLE: &'static str = "T";
@@ -106,58 +105,58 @@ impl<'a> From<&'a Macro> for MacroData<'a> {
 impl Macro {
     /// Returns a [`Vec`] of [`Sections`](crate::section::Section) representing the
     /// [`Macro`].
-    /// 
+    ///
     /// # Errors
-    /// 
+    ///
     /// Returns a [`DATError::ContentOverflow`] if the content of a section would exceed
     /// the maximum allowable length. ([`u16::MAX`]` - 1`)
-    /// 
+    ///
     /// # Examples
-    /// 
+    ///
     /// ```rust
     /// use libxivdat::xiv_macro::Macro;
     /// use libxivdat::xiv_macro::icon::MacroIcon;
     ///
     /// let a_macro = Macro::new(
-    ///     "Title".to_string(), 
+    ///     "Title".to_string(),
     ///     vec!["Circle".to_string()],
     ///     MacroIcon::SymbolCircle
     /// ).unwrap();
-    /// 
+    ///
     /// let sections = a_macro.as_sections().unwrap();
-    /// 
+    ///
     /// assert_eq!(sections[0].content, "Title");
     /// ```
     pub fn as_sections(&self) -> Result<Vec<Section>, DATError> {
         let mut sec_vec = vec![
             Section::new(SECTION_TAG_TITLE.to_owned(), String::from(&self.title))?,
             Section::new(SECTION_TAG_ICON.to_owned(), String::from(&self.icon_id))?,
-            Section::new(SECTION_TAG_KEY.to_owned(),String::from(&self.icon_key))?
+            Section::new(SECTION_TAG_KEY.to_owned(), String::from(&self.icon_key))?,
         ];
         for line in self.lines.iter() {
-            sec_vec.push(Section::new("L".to_owned(), String::from(line))?);
+            sec_vec.push(Section::new(SECTION_TAG_LINE.to_owned(), String::from(line))?);
         }
         Ok(sec_vec)
     }
 
     /// Changes the [`icon_key`](Self.icon_key) and [`icon_id`](Self.icon_id) to a valid pair based on an input
     /// [`MacroIcon`].
-    /// 
+    ///
     /// # Examples
-    /// 
+    ///
     /// ```rust
     /// use libxivdat::xiv_macro::Macro;
     /// use libxivdat::xiv_macro::icon::MacroIcon;
     ///
     /// let mut a_macro = Macro::new(
-    ///     "Title".to_string(), 
+    ///     "Title".to_string(),
     ///     Vec::<String>::new(),
     ///     MacroIcon::NoIcon
     /// ).unwrap();
-    /// 
+    ///
     /// assert_eq!(a_macro.icon_id, "0000000");
     /// assert_eq!(a_macro.icon_key, "000");
-    /// 
+    ///
     /// a_macro.change_icon(MacroIcon::SymbolArrowUp);
     /// assert_eq!(a_macro.icon_id, "00102FF");
     /// assert_eq!(a_macro.icon_key, "037");
@@ -188,7 +187,7 @@ impl Macro {
     /// the icon id and key specified are not a valid pair.
     ///
     /// Returns [`DATError::ContentOverflow`] if the title or any line is too long, or if there are too many lines.
-    /// 
+    ///
     /// Returns [`DATError::ContentUnderflow`] if there are too few lines.
     ///
     /// # Examples
@@ -262,7 +261,7 @@ impl Macro {
         let icon_key = String::from(&sections[2].content);
         let mut lines = Vec::<String>::new();
         for line in sections[3..].iter() {
-            if line.tag != "L" {
+            if line.tag != SECTION_TAG_LINE {
                 return Err(DATError::InvalidInput("Non-line (L) section in lines block."));
             }
             lines.push(line.content.to_owned());
@@ -277,15 +276,15 @@ impl Macro {
 
     /// Gets the [`MacroIcon`] correpsonding to the current [`icon_key`](Self.icon_key) and [`icon_id`](Self.icon_id).
     /// Returns [`None`] if the id and key do not correspond to a known valid icon.
-    /// 
+    ///
     /// # Examples
-    /// 
+    ///
     /// ```rust
     /// use libxivdat::xiv_macro::Macro;
     /// use libxivdat::xiv_macro::icon::MacroIcon;
     ///
     /// let a_macro = Macro::new(
-    ///     "Title".to_string(), 
+    ///     "Title".to_string(),
     ///     Vec::<String>::new(),
     ///     MacroIcon::SymbolArrowUp
     /// ).unwrap();
@@ -299,33 +298,33 @@ impl Macro {
     /// This ensures that the macro meets the spec described below, which is used
     /// by the game client. If the provided line count is less than 15, the count
     /// will be padded with blank lines.
-    /// 
+    ///
     /// To create an unvalidated [`Macro`], you can directly
     /// instantiate a struct literal.
-    /// 
+    ///
     /// # Macro spec
     ///
     /// Title: No more than 20 utf-8 characters.
     /// Icon: Icon id and key are a matching pair corresponding to a valid [`MacroIcon`].
     /// Lines: Exactly 15 lines of no more than 180 utf-8 characters.
-    /// 
+    ///
     /// # Errors
-    /// 
+    ///
     /// Returns [`DATError::ContentOverflow`] if the title or content are too long, or if there
     /// are too many lines.
-    /// 
+    ///
     /// # Examples
-    /// 
+    ///
     /// ```rust
     /// use libxivdat::xiv_macro::Macro;
     /// use libxivdat::xiv_macro::icon::MacroIcon;
     ///
     /// let a_macro = Macro::new(
-    ///     "Title".to_string(), 
+    ///     "Title".to_string(),
     ///     vec!["Circle".to_string()],
     ///     MacroIcon::SymbolCircle
     /// ).unwrap();
-    /// 
+    ///
     /// assert_eq!(a_macro.title, "Title");
     /// assert_eq!(a_macro.lines[0], "Circle");
     /// assert_eq!(a_macro.get_icon().unwrap(), MacroIcon::SymbolCircle);
@@ -342,11 +341,11 @@ impl Macro {
             icon_id: icon_id.to_owned(),
             icon_key: icon_key.to_owned(),
             lines: padded_lines,
-            title
+            title,
         };
         match res_macro.validate() {
             Some(err) => Err(err),
-            None => Ok(res_macro)
+            None => Ok(res_macro),
         }
     }
 
@@ -382,6 +381,310 @@ impl Macro {
     ///     icon_key: "XYZ".to_string(),
     ///     lines: vec![String::new(); 1],
     ///     title: "Looooooooooooooooong Title".to_string()
+    /// };
+    /// assert!(a_macro.validate().is_some());
+    /// ```
+    pub fn validate(&self) -> Option<DATError> {
+        if self.title.len() > 20 {
+            return Some(DATError::ContentOverflow("Title is longer than 20 characters."));
+        }
+        if macro_icon_from_key_and_id(&self.icon_key, &self.icon_id).is_none() {
+            return Some(DATError::InvalidInput("Macro icon is invalid."));
+        }
+        if self.lines.len() < 15 {
+            return Some(DATError::ContentUnderflow("Macro has fewer than 15 lines."));
+        }
+        if self.lines.len() > 15 {
+            return Some(DATError::ContentOverflow("Macro has more than 15 lines."));
+        }
+        for line in self.lines.iter() {
+            if line.len() > 180 {
+                return Some(DATError::ContentOverflow("Line is longer than 180 characters."));
+            }
+        }
+        None
+    }
+}
+
+impl<'a> MacroData<'a> {
+    /// Returns a [`Vec`] of [`SectionData`](crate::section::SectionData) representing the
+    /// [`MacroData`].
+    ///
+    /// # Errors
+    ///
+    /// Returns a [`DATError::ContentOverflow`] if the content of a section would exceed
+    /// the maximum allowable length. ([`u16::MAX`]` - 1`)
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use libxivdat::xiv_macro::MacroData;
+    /// use libxivdat::xiv_macro::icon::MacroIcon;
+    ///
+    /// let a_macro = MacroData::new(
+    ///     "Title",
+    ///     vec!["Circle"],
+    ///     &MacroIcon::SymbolCircle
+    /// ).unwrap();
+    ///
+    /// let sections = a_macro.as_section_data().unwrap();
+    ///
+    /// assert_eq!(sections[0].content, "Title");
+    /// ```
+    pub fn as_section_data(&self) -> Result<Vec<SectionData>, DATError> {
+        let mut sec_vec = vec![
+            SectionData::new(SECTION_TAG_TITLE, self.title)?,
+            SectionData::new(SECTION_TAG_ICON, self.icon_id)?,
+            SectionData::new(SECTION_TAG_KEY, self.icon_key)?,
+        ];
+        for line in self.lines.iter() {
+            sec_vec.push(SectionData::new(SECTION_TAG_LINE, line)?);
+        }
+        Ok(sec_vec)
+    }
+
+    /// Changes the [`icon_key`](Self.icon_key) and [`icon_id`](Self.icon_id) to a valid pair based on an input
+    /// [`MacroIcon`].
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use libxivdat::xiv_macro::MacroData;
+    /// use libxivdat::xiv_macro::icon::MacroIcon;
+    ///
+    /// let mut a_macro = MacroData::new(
+    ///     "Title",
+    ///     vec![""],
+    ///     &MacroIcon::NoIcon
+    /// ).unwrap();
+    ///
+    /// assert_eq!(a_macro.icon_id, "0000000");
+    /// assert_eq!(a_macro.icon_key, "000");
+    ///
+    /// a_macro.change_icon(&MacroIcon::SymbolArrowUp);
+    /// assert_eq!(a_macro.icon_id, "00102FF");
+    /// assert_eq!(a_macro.icon_key, "037");
+    /// ```
+    pub fn change_icon(&mut self, icon: &'a MacroIcon) -> () {
+        let (key, id) = macro_icon_to_key_and_id(&icon);
+        self.icon_key = key;
+        self.icon_id = id;
+    }
+
+    /// Builds a [`MacroData`] from a [`Vec`] of [`SectionData`](crate::section::SectionData).
+    /// The expected pattern of section tags is "T" (Title), "I" (Icon), "K", (Key), and repeating "L"s (Lines).
+    /// Valid macros always contain exactly 15 lines, even if their contents are blank. This function checks
+    /// the data for validity, unlick [`from_sections_unsafe()`](Self::from_sections_unsafe)
+    ///
+    /// This is equivalent to calling [`from_section_data_unsafe()`](Self::from_section_data_unsafe) followed by
+    /// [`validate()`](Self::validate) on the resulting [`Macro`].
+    ///
+    /// # Macro spec
+    ///
+    /// Title: No more than 20 utf-8 characters.
+    /// Icon: Icon id and key are a matching pair corresponding to a valid [`MacroIcon`].
+    /// Lines: Exactly 15 lines of no more than 180 utf-8 characters.
+    ///
+    /// # Errors
+    ///
+    /// Returns [`DATError::InvalidInput`] if the sections are not provided in the order described above or
+    /// the icon id and key specified are not a valid pair.
+    ///
+    /// Returns [`DATError::ContentOverflow`] if the title or any line is too long, or if there are too many lines.
+    ///
+    /// Returns [`DATError::ContentUnderflow`] if there are too few lines.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use libxivdat::section::SectionData;
+    /// use libxivdat::xiv_macro::MacroData;
+    ///
+    /// let mut sections = vec![
+    ///     SectionData { content: "Title", content_size: 6, tag: "T" },
+    ///     SectionData { content: "0000000", content_size: 8, tag: "I" },
+    ///     SectionData { content: "000", content_size: 4, tag: "K" }
+    /// ];
+    /// for line in std::iter::repeat("").take(15) {
+    ///     sections.push(SectionData { content: line, content_size: 1, tag: "L" });
+    /// }
+    /// let result_macro = MacroData::from_sections(sections).unwrap();
+    ///
+    /// assert_eq!(result_macro.title, "Title");
+    /// ```
+    pub fn from_sections(sections: Vec<SectionData>) -> Result<MacroData, DATError> {
+        let res_macro = Self::from_sections_unsafe(sections)?;
+        if let Some(validation_err) = res_macro.validate() {
+            Err(validation_err)
+        } else {
+            Ok(res_macro)
+        }
+    }
+
+    /// Builds a [`MacroData`] from a [`Vec`] of [`SectionData`](crate::section::SectionData).
+    /// The expected pattern of section tags is "T" (Title), "I" (Icon), "K", (Key), and repeating "L"s (Lines).
+    /// Valid macros always contain exactly 15 lines, even if their contents are blank. This library does not
+    /// strictly enforce this pattern, and will read lines until the next title.
+    ///
+    /// This function does not check that the actual section content is valid. To perform validity checks,
+    /// use [`from_sections()`](from_sections).
+    ///
+    /// # Errors
+    ///
+    /// Returns [`DATError::InvalidInput`] if the sections are not provided in the order described above.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use libxivdat::section::SectionData;
+    /// use libxivdat::xiv_macro::MacroData;
+    ///
+    /// let sections = vec![
+    ///     SectionData { content: "Title", content_size: 6, tag: "T" },
+    ///     SectionData { content: "0000000", content_size: 8, tag: "I" },
+    ///     SectionData { content: "000", content_size: 4, tag: "K" },
+    ///     SectionData { content: "A one line macro!?", content_size: 19, tag: "L" }
+    /// ];
+    /// let result_macro = MacroData::from_sections_unsafe(sections).unwrap();
+    ///
+    /// assert_eq!(result_macro.title, "Title");
+    /// assert_eq!(result_macro.lines.len(), 1);
+    /// ```
+    pub fn from_sections_unsafe(sections: Vec<SectionData>) -> Result<MacroData, DATError> {
+        if sections[0].tag != SECTION_TAG_TITLE {
+            return Err(DATError::InvalidInput("First section was not a Title (T) section."));
+        }
+        let title = sections[0].content;
+        if sections[1].tag != SECTION_TAG_ICON {
+            return Err(DATError::InvalidInput("Second section was not a Icon (I) section."));
+        }
+        let icon_id = sections[1].content;
+        if sections[2].tag != SECTION_TAG_KEY {
+            return Err(DATError::InvalidInput("Third section was not a Key (K) section."));
+        }
+        let icon_key = sections[2].content;
+        let mut lines = Vec::<&str>::new();
+        for line in sections[3..].iter() {
+            if line.tag != "L" {
+                return Err(DATError::InvalidInput("Non-line (L) section in lines block."));
+            }
+            lines.push(line.content);
+        }
+        Ok(MacroData {
+            icon_key,
+            icon_id,
+            lines,
+            title,
+        })
+    }
+
+    /// Gets the [`MacroIcon`] correpsonding to the current [`icon_key`](Self.icon_key) and [`icon_id`](Self.icon_id).
+    /// Returns [`None`] if the id and key do not correspond to a known valid icon.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use libxivdat::xiv_macro::MacroData;
+    /// use libxivdat::xiv_macro::icon::MacroIcon;
+    ///
+    /// let a_macro = MacroData::new(
+    ///     "Title",
+    ///     vec![""],
+    ///     &MacroIcon::SymbolArrowUp
+    /// ).unwrap();
+    /// assert_eq!(a_macro.get_icon().unwrap(), MacroIcon::SymbolArrowUp);
+    /// ```
+    pub fn get_icon(&self) -> Option<MacroIcon> {
+        macro_icon_from_key_and_id(&self.icon_key, &self.icon_id)
+    }
+
+    /// Builds a new [`MacroData`] with a given title, [`MacroIcon`], and content.
+    /// This ensures that the macro meets the spec described below, which is used
+    /// by the game client. If the provided line count is less than 15, the count
+    /// will be padded with blank lines.
+    ///
+    /// To create an unvalidated [`MacroData`], you can directly
+    /// instantiate a struct literal.
+    ///
+    /// # Macro spec
+    ///
+    /// Title: No more than 20 utf-8 characters.
+    /// Icon: Icon id and key are a matching pair corresponding to a valid [`MacroIcon`].
+    /// Lines: Exactly 15 lines of no more than 180 utf-8 characters.
+    ///
+    /// # Errors
+    ///
+    /// Returns [`DATError::ContentOverflow`] if the title or content are too long, or if there
+    /// are too many lines.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use libxivdat::xiv_macro::MacroData;
+    /// use libxivdat::xiv_macro::icon::MacroIcon;
+    ///
+    /// let a_macro = MacroData::new(
+    ///     "Title",
+    ///     vec!["Circle"],
+    ///     &MacroIcon::SymbolCircle
+    /// ).unwrap();
+    ///
+    /// assert_eq!(a_macro.title, "Title");
+    /// assert_eq!(a_macro.lines[0], "Circle");
+    /// assert_eq!(a_macro.get_icon().unwrap(), MacroIcon::SymbolCircle);
+    /// ```
+    pub fn new(title: &'a str, lines: Vec<&'a str>, icon: &'a MacroIcon) -> Result<MacroData<'a>, DATError> {
+        let (icon_key, icon_id) = macro_icon_to_key_and_id(&icon);
+        let mut padded_lines = lines.clone();
+        if lines.len() < 15 {
+            for line in std::iter::repeat("").take(15 - lines.len()) {
+                padded_lines.push(line);
+            }
+        }
+        let res_macro = MacroData {
+            icon_id: icon_id,
+            icon_key: icon_key,
+            lines: padded_lines,
+            title,
+        };
+        match res_macro.validate() {
+            Some(err) => Err(err),
+            None => Ok(res_macro),
+        }
+    }
+
+    /// Validates the macro against the spec expected by the game client.
+    /// Returns a [`DATError`] describing the error if validation fails, or [`None`]
+    /// if validation is successful.
+    ///
+    /// # Macro spec
+    ///
+    /// Title: No more than 20 utf-8 characters.
+    /// Icon: Icon id and key are a matching pair corresponding to a valid [`MacroIcon`].
+    /// Lines: Exactly 15 lines of no more than 180 utf-8 characters.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use libxivdat::xiv_macro::MacroData;
+    ///
+    /// let a_macro = MacroData {
+    ///     icon_id: "0000000",
+    ///     icon_key: "000",
+    ///     lines: vec![""; 15],
+    ///     title: "Title"
+    /// };
+    /// assert!(a_macro.validate().is_none());
+    /// ```
+    ///
+    /// ```rust
+    /// use libxivdat::xiv_macro::MacroData;
+    ///
+    /// let a_macro = MacroData {
+    ///     icon_id: "123456",
+    ///     icon_key: "XYZ",
+    ///     lines: vec![""; 1],
+    ///     title: "Looooooooooooooooong Title"
     /// };
     /// assert!(a_macro.validate().is_some());
     /// ```
